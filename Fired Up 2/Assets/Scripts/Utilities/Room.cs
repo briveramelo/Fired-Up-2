f4736b928239657ffce15dfb79259b6a369b41d0
naskew100 <naskew100@aol.com>
2015-12-05 13:55:17 -0700

    In Class Addition.  Start to Occlusion and Room Script
    In Class Addition.  Start to Occlusion and Room Script

---
+++ Fired Up 2/Assets/Scripts/Utilities/Room.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Room : MonoBehaviour {
+    public List<GameObject> startFires = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter()
+    {
+        //Undo Occulsion
+        //Set Fires
+
+    }
+    void OnTriggerExit()
+    {
+        //Enable Occulsion
+        //Stop Particles
+    }
+    void setFires()
+    {
+
+    }
+}
