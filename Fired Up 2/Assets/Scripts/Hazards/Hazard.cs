c4f28d92f606179bde37d408f1de71549c8ce138
naskew100 <naskew100@aol.com>
2015-12-07 20:44:17 -0700

    Hazards added.  Deaths for player now allowed.
    Hazards added.  Deaths for player now allowed.

---
+++ Fired Up 2/Assets/Scripts/Hazards/Hazard.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hazard : MonoBehaviour {
+    BoxCollider trigger;
+    float timeUnitlTrapFalls = 2f;
+   public bool active = true;
+	// Use this for initialization
+	void Start () {
+        trigger = this.GetComponent<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.name.Equals("FireFighter") && active)
+        {
+            Debug.Log("Trap Triggered");
+            
+                StartCoroutine(TrapCountdown());
+        }
+            
+    }
+    IEnumerator TrapCountdown()
+    {
+        Destroy(trigger);
+        active = false;
+        yield return new WaitForSeconds(timeUnitlTrapFalls);
+        ActivateTrap();
+        yield return null;
+    }
+    void ActivateTrap()
+    {
+        this.gameObject.AddComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name.Equals("FireFighter"))
+        {
+            Debug.Log("Player Died");
+        }
+    }
+}
