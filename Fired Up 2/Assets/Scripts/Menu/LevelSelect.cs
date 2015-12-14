e7d6d1f952caf82ab9b6186d46a238b76d18b1b1
Nathan Askew <naskew100@aol.com>
2015-12-14 12:55:40 -0700

    Tweaks to Oculus + level select.
    Tweaks to Oculus + level select.

--- Fired Up 2/Assets/Scripts/Menu/LevelSelect.cs
+++ Fired Up 2/Assets/Scripts/Menu/LevelSelect.cs
@@ -6,35 +6,74 @@ using System.Collections.Generic;
 public class LevelSelect : MonoBehaviour {
     public static int levelChoice;
     public int MyLevel;
-    Behaviour halo;
-    LevelSelect[] halos = new LevelSelect[5];
+    Light light;
+    public static Light chosenLight;
+    float startIntensity;
+    public bool isDefault;
+    LevelSelect[] lights = new LevelSelect[5];
 	// Use this for initialization
 	void Start () {
-        halo = (Behaviour)GetComponent("Halo");
-        halos = FindObjectsOfType<LevelSelect>();
+        
+        light = GetComponent<Light>();
+        lights = FindObjectsOfType<LevelSelect>();
+        startIntensity = light.intensity;
+        Debug.Log(startIntensity);
+        if (!isDefault)
+            light.intensity = 0;
+        else
+        {
+            chosenLight = light;
+            levelChoice = MyLevel;
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-    void OnMouseOver()
+    public void OnHoverOverObject()
     {
-        halo.enabled = true;
-    }
-    void OnMouseExit()
-    {
-        if(MyLevel != levelChoice)
-        halo.enabled = false;
-    }
-    void OnMouseDown()
-    {
-        for (int i = 0; i < halos.Length; i++)
+        light.intensity = startIntensity;
+        StartCoroutine(ChangeLight(1,light));
+        if (chosenLight != light)
         {
-            halos[i].halo.enabled = false;
+            StartCoroutine(ChangeLight(-.75f, chosenLight));
         }
-        halo.enabled = true;
+
+      
+    }
+    public void OnHoverExitObject()
+    {
+        StopAllCoroutines();
+        Debug.Log(levelChoice);
+        if (MyLevel != levelChoice)
+        {
+
+            light.intensity = 0;
+        }
+        else
+            light.intensity = startIntensity;
+            
+    }
+    public void OnHoldForEnoughTime()
+    {
+        for (int i = 0; i < lights.Length; i++)
+        {
+            if (lights[i].MyLevel != MyLevel)
+            lights[i].light.intensity = 0;
+
+        }
+        //light.intensity = startIntensity;
+       // StartCoroutine(ChangeLight(1, light));
         levelChoice = MyLevel;
+        chosenLight = light;
+    }
+    public IEnumerator ChangeLight(float LightOrDark, Light light)
+    {
+        Debug.Log(light.name + "  " + LightOrDark);
+        light.intensity = Mathf.Clamp(light.intensity + (LightOrDark * Time.deltaTime),0,3);
+        light.range = Mathf.Clamp(light.range + (LightOrDark * Time.deltaTime), 0, 4);
+        yield return new WaitForSeconds(.1f);
     }
 
 }
---
+++ Fired Up 2/Assets/Scripts/UI/OctulusReticle.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OctulusReticle : MonoBehaviour {
+    Renderer material;
+	// Use this for initialization
+	void Start () {
+        material = this.GetComponent<Renderer>();
+        material.material.SetFloat("_Cutoff", .01f);
+    }
+	
+
+    void OnTriggerStay(Collider col)
+    {
+        material.material.SetFloat("_Cutoff", material.material.GetFloat("_Cutoff") + .4f * Time.deltaTime);
+        if (col.gameObject.GetComponent<DifficultySelect>() != null)
+        {
+            col.gameObject.GetComponent<DifficultySelect>().OnHoverOverObject();
+            if (material.material.GetFloat("_Cutoff") >= 1)
+            {
+
+                col.gameObject.GetComponent<DifficultySelect>().OnHoldForEnoughTime();
+            }
+            
+        }
+        else if (col.gameObject.GetComponent<LevelSelect>() != null)
+        {
+            col.gameObject.GetComponent<LevelSelect>().OnHoverOverObject();
+            if (material.material.GetFloat("_Cutoff") >= 1)
+            {
+
+                col.gameObject.GetComponent<LevelSelect>().OnHoldForEnoughTime();
+            }
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.GetComponent<DifficultySelect>() != null)
+        {
+            col.gameObject.GetComponent<DifficultySelect>().OnHoverExitObject();
+        }
+        else if (col.gameObject.GetComponent<LevelSelect>() != null)
+        {
+            col.gameObject.GetComponent<LevelSelect>().OnHoverExitObject();
+        }
+            material.material.SetFloat("_Cutoff", .01f);
+    }
+}
